2d3eaba3f360b3184aad802cd92d3e7883ea499b
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-04-21 22:34:56 -0300

    test: replace ts-jest/utils to only ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 const nextJest = require("next/jest");
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { pathsToModuleNameMapper } = require("ts-jest");
 
 const { compilerOptions } = require("./tsconfig.json");
 
